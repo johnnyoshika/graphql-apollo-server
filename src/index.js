a0510984b86454413c6110914ba3cded108faae2
Johnny Oshika <johnnyoshika@gmail.com>
2020-03-30 14:31:06 -0700

    Seed the database

--- src/index.js
+++ src/index.js
@@ -13,11 +13,52 @@ app.use(cors());
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  context: {
+    models,
+    // me: models.users[1],
+  },
 });
 server.applyMiddleware({ app, path: '/graphql' });
 
-sequelize.sync().then(async () => {
-  app.listen({ port: 8000 }, () => {
-    console.log('Apollo Server on http://localhost:8000/graphql');
+const eraseDatabaseOnSync = true;
+
+sequelize
+  .sync({
+    force: eraseDatabaseOnSync,
+  })
+  .then(async () => {
+    if (eraseDatabaseOnSync) createUsersWithMessages();
+
+    app.listen({ port: 8000 }, () => {
+      console.log('Apollo Server on http://localhost:8000/graphql');
+    });
   });
-});
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'louferigno',
+      messages: [
+        {
+          text: 'The ultimate hulk!',
+        },
+      ],
+    },
+    { include: [models.Message] },
+  );
+
+  await models.User.create(
+    {
+      username: 'bradpitt',
+      messages: [
+        {
+          text: 'A legend of the fall',
+        },
+        { text: '12 monkeys!' },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+};
