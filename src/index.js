50d6ca04c524cf7c76c4200a02cce9daf50070ac
Johnny Oshika <johnnyoshika@gmail.com>
2020-03-30 13:46:38 -0700

    Sync Sequelize models with Postgres database on startup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { ApolloServer } from 'apollo-server-express';
 
 import schema from './schema';
 import resolvers from './resolvers';
-import models from './models';
+import models, { sequelize } from './models';
 
 const app = express();
 app.use(cors());
@@ -13,13 +13,11 @@ app.use(cors());
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  context: {
-    models,
-    me: models.users[1]
-  }
 });
-
 server.applyMiddleware({ app, path: '/graphql' });
-app.listen({ port: 8000 }, () => {
-  console.log('Apollo Server on http://localhost:8000/graphql');
+
+sequelize.sync().then(async () => {
+  app.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql');
+  });
 });
