97d22ff4b20a8e625b6b3ea8ceae687fe89c6b69
Johnny Oshika <johnnyoshika@gmail.com>
2020-03-31 10:41:11 -0700

    Add validation

--- src/models/message.js
+++ src/models/message.js
@@ -2,6 +2,7 @@ const message = (sequelize, DataTypes) => {
   const Message = sequelize.define('messages', {
     text: {
       type: DataTypes.STRING,
+      validate: { notEmpty: true },
     },
   });
 
