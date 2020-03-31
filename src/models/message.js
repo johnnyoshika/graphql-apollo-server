c41f494b277677313c0a943952b8957b718acd21
Johnny Oshika <johnnyoshika@gmail.com>
2020-03-31 10:44:28 -0700

    Add custom validation message

--- src/models/message.js
+++ src/models/message.js
@@ -2,7 +2,12 @@ const message = (sequelize, DataTypes) => {
   const Message = sequelize.define('messages', {
     text: {
       type: DataTypes.STRING,
-      validate: { notEmpty: true },
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'text is required',
+        },
+      },
     },
   });
 
